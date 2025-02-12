08b9ac2eb97c540b6ee0c193aceb964254386ded
Jairo Miguel <78458400+jmitsuu@users.noreply.github.com>
2025-02-12 17:12:17 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './App.css';
-import { Toaster } from '@/components/ui/sonner';
-import { Header } from './components/header/header';
-import { ShoppRoutes } from './routes/router';
-import { Footer } from './components/footer/footer';
-import { BrowserRouter as Router } from 'react-router-dom';
-import ScrollToTop from './routes/scrollToUp';
+import "./index.css";
+import { AppRouter } from "./AppRouter";
+import { CartStore } from "./store/CartStore";
+import { Toaster } from "@/components/ui/toaster";
 
-const queryClient = new QueryClient();
 function App() {
-  return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <ScrollToTop />
-          <Header />
-          <ShoppRoutes />
-          <Footer />
-        </Router>
-        <Toaster />
-      </QueryClientProvider>
-    </>
-  );
+ return (
+  <>
+   <CartStore>
+    <AppRouter />
+    <Toaster />
+   </CartStore>
+  </>
+ );
 }
 
 export default App;
